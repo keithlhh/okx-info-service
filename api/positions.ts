3e43c42dbb4b686400f74d432c0b7fa351fd00db
keith <44639634+keithlhh@users.noreply.github.com>
2024-06-27 22:44:51 +0800

    Update positions.ts

--- api/positions.ts
+++ api/positions.ts
@@ -2,9 +2,14 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import axios from 'axios';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { uniqueName = '563E3A78CDBAFB4E' } = req.query
+  const { uniqueName = '563E3A78CDBAFB4E', limit = 100 } = req.query
   try {
-    const response = await axios.get(`https://www.okx.com/priapi/v5/ecotrade/public/positions-v2?uniqueName=${uniqueName}`);
+    const response = await axios.get(`https://www.okx.com/priapi/v5/ecotrade/public/positions-v2?uniqueName=${uniqueName}&limit={limit}`);
+
+
+
+
+
     res.json(response.data);
   } catch (error) {
       console.error(error);
