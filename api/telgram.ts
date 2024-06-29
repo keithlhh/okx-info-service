134f4e0baa551140b6ff72c4748fa105bce6a728
ex_lihh14 <ex_lihh14@partner.midea.com>
2024-06-29 08:40:05 +0800

    update1

--- api/telgram.ts
+++ api/telgram.ts
@@ -25,31 +25,33 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
   try {
     global.start = true;
-    global.timer = setInterval(async () => {
-      const response = await axios.get(`https://www.okx.com/priapi/v5/ecotrade/public/positions-v2?uniqueName=${uniqueName}`);
-      res.json({...response.data, count: global.count});
-      const data = response?.data?.data;
-      const strData = JSON.stringify(data);
-      const test = new Date().getSeconds();
-      // if (test == 30) {
-        global.count += 1;
-        axios.post(
-          "https://api.telegram.org/bot7456345325:AAGydyNYEeAXeNmJrxYmHY5zT3iNqlR6ycI/sendMessage",
-          {
-            chat_id: "1604598018",
-            text: new Date() + " " + "正在运行中..." + global.count,
-          }
-        );
-      // }
-      if (strData !== global.prev) {
-        axios.post(
-          "https://api.telegram.org/bot7456345325:AAGydyNYEeAXeNmJrxYmHY5zT3iNqlR6ycI/sendMessage",
-          {
-            chat_id: "-4259724953",
-            text: strData,
-          }
-        );
-      }
+    global.timer = setInterval(() => {
+      res.json({count: global});
+      axios.get(`https://www.okx.com/priapi/v5/ecotrade/public/positions-v2?uniqueName=${uniqueName}`).then((ret) => {
+        const response = ret;
+        const data = response?.data?.data;
+        const strData = JSON.stringify(data);
+        const test = new Date().getSeconds();
+        if (test == 30) {
+          global.count += 1;
+          axios.post(
+            "https://api.telegram.org/bot7456345325:AAGydyNYEeAXeNmJrxYmHY5zT3iNqlR6ycI/sendMessage",
+            {
+              chat_id: "1604598018",
+              text: new Date() + " " + "正在运行中..." + global.count,
+            }
+          );
+        }
+        if (strData !== global.prev) {
+          axios.post(
+            "https://api.telegram.org/bot7456345325:AAGydyNYEeAXeNmJrxYmHY5zT3iNqlR6ycI/sendMessage",
+            {
+              chat_id: "-4259724953",
+              text: strData,
+            }
+          );
+        }
+      })
     }, 4000)
   } catch (error) {
       console.error(error);
